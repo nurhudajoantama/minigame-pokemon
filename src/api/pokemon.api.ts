fa26e2b33580b5e5f0116fbf74d9c0d2aabf0dc0
nurhudajoantama <nurhuda.joantama14@gmail.com>
2025-06-10 10:07:24 +0700

    fix: fix camelcase import on index css

--- src/api/pokemon.api.ts
+++ src/api/pokemon.api.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+
 import { usePokemon } from "../providers/PokemonProvider";
 import type { AllPokemonItem, PokemonDetail } from "../types/pokemon";
 
